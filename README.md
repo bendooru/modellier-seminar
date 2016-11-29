8b6a67988afc38ccccaa6eb3020db77c1aecc3ff
bendooru <bendooru@users.noreply.github.com>
2016-11-29 12:17:07 +0100

    Readme, Matlab

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# modellier-seminar
+# Moellierungsseminar
+
+Repository zur Speicherung relevanter Dateien:
+
+* `erstes_modell` enthält Matlab-Dateien des Sonnen-Modells.
+* `vorstellung` enthält Dateien der Projektvorstellung.
+* `zwischenber1` enthält Dateien zum ersten Zwischenbericht.
--- erstes_modell/earth_path.m
+++ erstes_modell/earth_path.m
@@ -1,11 +1,9 @@
-function X = earth_path(lon, lat, t, delta_t, v, steps)
+function X = earth_path(lon, lat, t, delta_t, v, steps, radius)
     X = zeros(3,steps);
-    
-    earth_rad = 6371000; % Meter
     
     p_0 = zeros(3,1);
     
-    [p_0(1), p_0(2), p_0(3)] = sph2cart(lon, lat, earth_rad);
+    [p_0(1), p_0(2), p_0(3)] = sph2cart(lon, lat, radius);
     
     X(:,1) = p_0;
     
@@ -28,7 +26,7 @@ function X = earth_path(lon, lat, t, delta_t, v, steps)
             end
 
             p = p_prev + v*delta_t*r_normal;
-            p = (earth_rad/norm(p)) .* p;
+            p = (radius/norm(p)) .* p;
         else
             p = p_prev;
         end
