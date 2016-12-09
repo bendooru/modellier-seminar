591f1a23472bcc735fa05a5db92a98a10ce1609c
bendooru <bendooru@users.noreply.github.com>
2016-12-09 02:28:59 +0100

    Winzige Verschönerung

--- erstes_modell/osm_test.m
+++ erstes_modell/osm_test.m
@@ -1,16 +1,19 @@
+% Erste Versuche
+
 bounds = [50.74 7.15 50.75 7.16];
 
 tic
-api_name = ['http://overpass-api.de/api/interpreter?data=(way(' ...
-    num2str(bounds(1)) ',' num2str(bounds(2)) ',' num2str(bounds(3)) ...
-    ',' num2str(bounds(4)) ');>;);out;'];
+api_name = sprintf(...
+    'http://overpass-api.de/api/interpreter?data=(way(%f,%f,%f,%f);>;);out;',...
+    bounds);
 
 remote_xml = webread(api_name);
 toc
 indices = strfind(remote_xml, '<node');
 index = indices(1);
 
-remote_xml = sprintf('%s<bounds minlat="%f" minlon="%f" maxlat="%f" maxlon="%f"/>\n  %s', ...
+remote_xml = sprintf(...
+    '%s<bounds minlat="%f" minlon="%f" maxlat="%f" maxlon="%f"/>\n  %s', ...
     remote_xml(1:index-1), bounds, remote_xml(index:end));
 
 %filename = '../../OSMmatlab/map.osm';
