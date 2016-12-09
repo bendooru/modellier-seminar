8a7293239e6b665779ac3c044953b68a6903336a
bendooru <bendooru@users.noreply.github.com>
2016-12-09 15:59:30 +0100

    findNearestVec vergessen

---
+++ erstes_modell/findNearestVec.m
@@ -0,0 +1,11 @@
+function idx = findNearestVec(v, A)
+    % findet Vektor mit geringster Distanz zu v in A, wobei v ein mx1-Vektor ist
+    % und A eine mxn-Matrix bestehend aus n mx1-Vektoren
+    if size(v, 1) == size(A, 1)
+        % spaltenweise euklidische Norm
+        [~, idx] = min(sum((A - repmat(v,1,size(A,2))).^2, 1));
+    else
+        warning('Wrong vector or matrix dimension in findNearest2.');
+        idx = NaN;
+    end
+end
