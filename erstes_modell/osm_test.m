ffa2de55fd20fa1d3e63fe79aace57d677964308
bendooru <bendooru@users.noreply.github.com>
2016-12-10 19:36:22 +0100

    Inhalt nach follow_osm verschoben

--- erstes_modell/osm_test.m
+++ erstes_modell/osm_test.m
@@ -1,42 +1,23 @@
-% Erste Versuche
 
-bounds = [50.12 7.2 50.14 7.24];
+% % Kaiserslautern irgendwo
+% lon =  7.768889;
+% lat = 49.444722;
 
-tic
-% API-Query muss noch angepasst werden
-api_name = sprintf(...
-    'http://overpass-api.de/api/interpreter?data=(way["highway"](%f,%f,%f,%f);>;);out;',...
-    bounds);
+% Tokio: rechnet lange!
+lon = 139.774444;
+lat =  35.683889;
 
-remote_xml = webread(api_name);
-toc
-indices = strfind(remote_xml, '<node');
-index = indices(1);
+% % TU KL
+% lon =  7.753056;
+% lat = 49.423889;
 
-remote_xml = sprintf(...
-    '%s<bounds minlat="%f" minlon="%f" maxlat="%f" maxlon="%f"/>\n  %s', ...
-    remote_xml(1:index-1), bounds, remote_xml(index:end));
+% lon =  7.735440;
+% lat = 49.43914;
 
-%filename = '../../OSMmatlab/map.osm';
-filename = sprintf('maps/map-%f_%f_%f_%f.osm', bounds);
-fid = fopen(filename, 'wt');
-fprintf(fid, '%s', remote_xml);
-fclose(fid);
-%%
+delta_t = 1;
+speed = 90; % [m/min]
 
-tic;
-[parsed_osm, osm_xml] = parse_openstreetmap(filename);
+% fehlt: day.m integrieren
+tag = 172;
 
-[connectivity_matrix, intersection_node_indices] = extract_connectivity(parsed_osm);
-toc;
-
-dg = or(connectivity_matrix, connectivity_matrix.');
-
-%%
-
-curr_coord = [7.2; 50.14]; % Spaltenvektor!
-
-nearest_node_idx = findNearestVec(curr_coord, parsed_osm.node.xy);
-
-possible_next = find(dg(:, nearest_node_idx)); % Indizes adjazenter Knoten
-
+X = follow_osm(lon, lat, delta_t, speed, tag);
