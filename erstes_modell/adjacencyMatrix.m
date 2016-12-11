b1d9e4a22febe4caa2560e0a27213c27e9005da3
bendooru <bendooru@users.noreply.github.com>
2016-12-11 16:57:25 +0100

    Mache Ausgabe symmetrisch

--- erstes_modell/adjacencyMatrix.m
+++ erstes_modell/adjacencyMatrix.m
@@ -5,7 +5,7 @@ function adjMat = adjacencyMatrix(parsed_osm)
     ways_node_sets = way.nd;
     node_ids = node.id;
     
-    adjMat = sparse(size(node_ids, 2), size(node_ids, 2));
+    adjMat = sparse(size(node_ids, 2));
     
     for currway = 1:ways_num
         nodeset = ways_node_sets{1, currway};
@@ -18,9 +18,15 @@ function adjMat = adjacencyMatrix(parsed_osm)
         
         for i = 1:size(nodeset, 2)
             idx = node_ids == nodeset(1,i);
+            % Matrix bereits an dieser Stelle symmetrisch machen
             adjMat(idx_prev, idx) = 1;
+            adjMat(idx, idx_prev) = 1;
             
             idx_prev = idx;
         end
+    end
+    
+    for i = 1:size(node_ids, 2)
+        adjMat(i,i) = 0;
     end
 end
