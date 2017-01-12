ea2344fa37184e57e8e95615a874a4d0e5440387
bendooru <bendooru@users.noreply.github.com>
2017-01-12 13:50:29 +0100

    Neues Modell

--- erstes_modell/osm_test.m
+++ erstes_modell/osm_test.m
@@ -3,13 +3,15 @@
 % lon =  7.768889;
 % lat = 49.444722;
 
+lat = 49.439343; lon = 7.733482;
+
 % % Tokio: rechnet lange!
 % lon = 139.774444;
 % lat =  35.683889;
 
-% TU KL
-lon =  7.753056;
-lat = 49.423889;
+% % TU KL
+% lon =  7.753056;
+% lat = 49.423889;
 
 % lon =  7.311105;
 % lat = 49.712424;
@@ -21,9 +23,11 @@ lat = 49.423889;
 % lat = 35;
 
 delta_t = 1;
-speed = 90; % [m/min]
+
+fitness.walkpause = [180 30 100 5 200 27 30 5];
+fitness.f = { @(t) 20*exp(-t) + 65 };
 
 % Format: tt, mm Tag, Monat
 tag = day(21, 6);
 
-[X, ax] = follow_osm(lon, lat, delta_t, speed, tag);
+[X, ax, D, T] = follow_osm(lon, lat, delta_t, tag, fitness, 'TimePlot');
