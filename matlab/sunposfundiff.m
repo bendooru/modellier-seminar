fca44cd0a925732d56cc3e094434d4c47c6b8d57
bendooru <bendooru@users.noreply.github.com>
2017-02-03 20:11:54 +0100

    Methode, Sonnenstand-Funktionen zu vergleichen

---
+++ matlab/sunposfundiff.m
@@ -0,0 +1,9 @@
+setenv('MATLAB_SUNPOSITION_FUN', '');
+osm_test; clc;
+
+plot(X(1, :), X(2, :)); hold on;
+
+setenv('MATLAB_SUNPOSITION_FUN', 'exact');
+osm_test;
+
+plot(X(1, :), X(2, :)); hold off;
