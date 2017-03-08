a2e38f7c5ec13ba84e6e96f9d9f239aa04438240
bendooru <bendooru@users.noreply.github.com>
2017-03-08 23:55:53 +0100

    Mache externe Resourcen nutzbar

---
+++ prepare_resources.m
@@ -0,0 +1,27 @@
+% füge nötige Dateien dem Pfad hinzu, installiere Toolbox
+% ... hoffentlich
+
+basepath = fileparts(mfilename('fullpath'));
+
+% verwende exemplarisch openstreetmap als externe Funktion
+if exist('parse_openstreetmap', 'file') ~= 2
+    paths = { 'readhgt', 'solar' };
+    osmpaths = fullfile('osmfun', ...
+        { 'dependencies', 'gaimc', 'hold', 'lat_lon_proportions', ...
+          'openstreetmapfunctions', 'plotmd', 'xml2struct' });
+
+    allpaths = fullfile(basepath, 'externe_resourcen', [paths, osmpaths]);
+    
+    addpath(allpaths{:}, '-end');
+end
+
+if exist('uiextras.HBox', 'class') ~= 8
+    guipaths = fullfile(basepath, 'guilayout', {'', 'layout', 'layoutdoc'});
+    addpath(guipaths{:}, '-end');
+end
+
+% Schließlich eigene Funktionen
+if exist('SonneGUI', 'file') ~= 2
+    addpath(fullfile(basepath, 'matlab'), '-end');
+end
+    
