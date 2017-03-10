4b3f9f3bea764ac2b0f6473aebedba2f57d474ec
bendooru <bendooru@users.noreply.github.com>
2017-03-10 01:17:29 +0100

    Prüfe, ob Submodul existiert.

--- prepare_resources.m
+++ prepare_resources.m
@@ -3,9 +3,9 @@
 
 basepath = fileparts(mfilename('fullpath'));
 
-% verwende exemplarisch openstreetmap als externe Funktion
-if exist('parse_openstreetmap', 'file') ~= 2
-    paths = { 'readhgt', 'solar', 'openstreetmap' };
+% verwende exemplarisch xml2struct als externe Funktion
+if exist('xml2struct_fex28518', 'file') ~= 2
+    paths = { 'readhgt', 'solar' };
     osmpaths = fullfile('dependencies', ...
         { 'gaimc', 'hold', 'lat_lon_proportions', 'plotmd', 'xml2struct' });
 
@@ -14,6 +14,22 @@ if exist('parse_openstreetmap', 'file') ~= 2
     addpath(allpaths{:}, '-end');
 end
 
+if exist('parse_openstreetmap', 'file') ~= 2
+    osmpath = fullfile(basepath, 'externe_resourcen', 'openstreetmap');
+    if ~isdir(osmpath)
+        zipfile = fullfile(basepath, 'openstreetmap-0.3.zip');
+        if exist(zipfile, 'file') == 2
+            unzip(zipfile, osmpath)
+        end
+    end
+
+    if isdir(osmpath)
+        addpath(osmpath, '-end')
+    else
+        warning('openstreetmap-Ordner nicht vorhanden!');
+        warning('Platziere openstreetmap-v0.3.zip in diesem Ordner.'); 
+    end
+end
 if exist('uiextras.HBox', 'class') ~= 8
     guipaths = fullfile(basepath, 'guilayout', {'', 'layout', 'layoutdoc'});
     addpath(guipaths{:}, '-end');
